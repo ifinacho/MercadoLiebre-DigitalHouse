fcf80e677363590c793fcc66c367ce4d63ee6906
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2024-01-29 20:11:12 -0300

    Cambiando el argumento de static public

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
+
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
